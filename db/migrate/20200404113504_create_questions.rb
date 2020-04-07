263af8a4270ded73c73ba5afae0710ba60a47898
utopia22 <sekishinya@mba.local>
2020-04-07 21:15:52 +0900

    _body_css

--- db/migrate/20200404113504_create_questions.rb
+++ db/migrate/20200404113504_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
       t.text :title
-      t.text :detail
+      t.text :body
 
       t.timestamps
     end
