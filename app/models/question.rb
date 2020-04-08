9d6bea4bd33834c11bc2edd24cd43a6e6268ba3e
utopia22 <sekishinya@mba.local>
2020-04-08 20:52:32 +0900

    controller

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,12 +1,2 @@
 class Question < ApplicationRecord
-
-  def self.import(path)
-     CSV.foreach(path, headers: true) do |row|
-       Question.create(
-         title: row["title"],
-         body: row["body"],
-       )
-     end
-  end
-
 end
