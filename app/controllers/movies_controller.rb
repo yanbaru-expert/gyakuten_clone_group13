71849b87ddf63e7c86016192fb55d61e639f1085
奈良亮 <motorbicykle@gmail.com>
2020-04-29 14:25:13 +0900

    動画コンテンツ実装の修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,7 @@ class MoviesController < ApplicationController
     if params[:category]
       @movies = Movie.where(category: params[:category]).page(params[:page]).per(10)
     else
-      @movies = Movie.where(category: "Ruby on Rails").page(params[:page]).per(10)
+      @movies = Movie.where(category: ["Basic", "Git", "Ruby", "Ruby on Rails"]).page(params[:page]).per(10)
     end
   end
 
