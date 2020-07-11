dfeb7bc5c914f5837bc122dcd59115edc4826930
sekishinya <sekishinya@example.com>
2020-07-11 20:25:53 +0900

    movies.controller_new以下削除

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,31 +8,4 @@ class MoviesController < ApplicationController
     end
   end
 
-  def new
-    @movie = Movie.new
-  end
-
-  def create
-    @movie = Movie.new(movie_params)
-    url = params[:movie][:url]
-    url = url.last(11)
-    @movie.url = url
-
-    respond_to do |format|
-      if @movie.save
-        format.html { redirect_to @movie, notice: 'Movie was successfully created.' }
-        format.json { render :show, status: :created, location: @movie}
-      else
-        format.html { render :new }
-        format.json { render json: @movie.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  private
-
-    def movie_params
-      params.require(:movie).permit(:category, :title, :url)
-    end
-
 end
